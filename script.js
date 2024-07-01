a380a5ca0ef6bc3866acd4da3d8e9fc4ca83d03d
Aravindhan <aravindhan12072000@gmail.com>
2024-07-01 18:36:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,468 @@
+let quizData = [
+  {
+    sectionTitle: "General Knowledge",
+    questions: [
+      {
+        questionType: "mcq",
+        question: "What is the capital of Australia?",
+        options: ["Sydney", "Melbourne", "Canberra", "Perth"],
+        answer: "Canberra",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "Who is known as the 'Father of Computers'?",
+        options: [
+          "Charles Babbage",
+          "Alan Turing",
+          "Tim Berners-Lee",
+          "Bill Gates",
+        ],
+        answer: "Charles Babbage",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "Which planet is known as the 'Red Planet'?",
+        options: ["Venus", "Mars", "Jupiter", "Saturn"],
+        answer: "Mars",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the chemical symbol for sodium?",
+        options: ["Na", "So", "Sn", "Nb"],
+        answer: "Na",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the largest mammal in the world?",
+        options: ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+        answer: "Blue Whale",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "Which country is known as the 'Land of the Rising Sun'?",
+        options: ["Japan", "China", "South Korea", "India"],
+        answer: "Japan",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the chemical symbol for gold?",
+        options: ["Au", "Ag", "Fe", "Cu"],
+        answer: "Au",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the tallest mammal in the world?",
+        options: ["Giraffe", "Elephant", "Horse", "Lion"],
+        answer: "Giraffe",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the largest continent by land area?",
+        options: ["Asia", "Africa", "North America", "Europe"],
+        answer: "Asia",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "Who wrote the play 'Romeo and Juliet'?",
+        options: [
+          "William Shakespeare",
+          "Jane Austen",
+          "Emily Bronte",
+          "Charles Dickens",
+        ],
+        answer: "William Shakespeare",
+        check: false,
+      },
+    ],
+  },
+  {
+    sectionTitle: "Technology",
+    questions: [
+      {
+        questionType: "mcq",
+        question: "Who is the CEO of Tesla, Inc.?",
+        options: ["Elon Musk", "Jeff Bezos", "Tim Cook", "Mark Zuckerberg"],
+        answer: "Elon Musk",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "Which company developed the first graphical web browser?",
+        options: ["Netscape", "Microsoft", "Google", "Mozilla"],
+        answer: "Netscape",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What does 'CPU' stand for in computing?",
+        options: [
+          "Central Processing Unit",
+          "Computer Processing Unit",
+          "Central Processor Unit",
+          "Control Processing Unit",
+        ],
+        answer: "Central Processing Unit",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question:
+          "Which programming language is commonly used for building Android apps?",
+        options: ["Java", "Python", "C++", "Swift"],
+        answer: "Java",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "Who is known as the 'Father of Computer Science'?",
+        options: [
+          "Alan Turing",
+          "Charles Babbage",
+          "Tim Berners-Lee",
+          "Steve Jobs",
+        ],
+        answer: "Alan Turing",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question:
+          "Which company developed the first commercially successful microprocessor?",
+        options: ["Intel", "AMD", "IBM", "Apple"],
+        answer: "Intel",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What does 'HTML' stand for?",
+        options: [
+          "Hypertext Markup Language",
+          "Hyperlink Text Markup Language",
+          "Hypertext Transfer Markup Language",
+          "Hyperlink Transfer Markup Language",
+        ],
+        answer: "Hypertext Markup Language",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question:
+          "What is the term used to describe the process of making a computer program bug-free?",
+        options: ["Debugging", "Compiling", "Testing", "Coding"],
+        answer: "Debugging",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the smallest unit of data in a computer?",
+        options: ["Bit", "Byte", "Kilobyte", "Megabyte"],
+        answer: "Bit",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "Which company developed the first smartphone?",
+        options: ["IBM", "Motorola", "Apple", "Nokia"],
+        answer: "IBM",
+        check: false,
+      },
+    ],
+  },
+  {
+    sectionTitle: "Science",
+    questions: [
+      {
+        questionType: "mcq",
+        question: "What is the chemical symbol for water?",
+        options: ["H2O", "CO2", "O2", "NaCl"],
+        answer: "H2O",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "Which gas is most abundant in Earth's atmosphere?",
+        options: ["Nitrogen", "Oxygen", "Carbon Dioxide", "Argon"],
+        answer: "Nitrogen",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the atomic number of carbon?",
+        options: ["5", "6", "7", "8"],
+        answer: "6",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the unit of electric current?",
+        options: ["Volt", "Watt", "Ampere", "Ohm"],
+        answer: "Ampere",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "Which planet is known as the 'Morning Star'?",
+        options: ["Venus", "Mars", "Jupiter", "Saturn"],
+        answer: "Venus",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the smallest bone in the human body?",
+        options: ["Femur", "Tibia", "Stapes", "Radius"],
+        answer: "Stapes",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the process by which plants make their food?",
+        options: [
+          "Photosynthesis",
+          "Respiration",
+          "Transpiration",
+          "Pollination",
+        ],
+        answer: "Photosynthesis",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the study of earthquakes called?",
+        options: ["Meteorology", "Seismology", "Volcanology", "Geology"],
+        answer: "Seismology",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "Which element has the highest melting point?",
+        options: ["Gold", "Iron", "Platinum", "Tungsten"],
+        answer: "Tungsten",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question:
+          "What is the process by which water vapor turns into liquid water?",
+        options: ["Evaporation", "Condensation", "Sublimation", "Freezing"],
+        answer: "Condensation",
+        check: false,
+      },
+    ],
+  },
+  {
+    sectionTitle: "Mathematics",
+    questions: [
+      {
+        questionType: "mcq",
+        question: "What is the value of 2 + 2 * 2?",
+        options: ["4", "6", "8", "10"],
+        answer: "6",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the square root of 64?",
+        options: ["4", "7", "8", "10"],
+        answer: "8",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the value of π (pi) rounded to two decimal places?",
+        options: ["3.12", "3.14", "3.16", "3.18"],
+        answer: "3.14",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the next number in the sequence: 2, 4, 6, 8, ...?",
+        options: ["10", "12", "14", "16"],
+        answer: "10",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the product of 7 multiplied by 9?",
+        options: ["54", "63", "72", "81"],
+        answer: "63",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the sum of angles in a triangle?",
+        options: ["90 degrees", "120 degrees", "180 degrees", "360 degrees"],
+        answer: "180 degrees",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the area of a square with side length 5 units?",
+        options: [
+          "20 square units",
+          "25 square units",
+          "30 square units",
+          "35 square units",
+        ],
+        answer: "25 square units",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the value of 3 to the power of 4?",
+        options: ["9", "12", "27", "81"],
+        answer: "81",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the sum of the first 10 positive integers?",
+        options: ["45", "50", "55", "60"],
+        answer: "55",
+        check: false,
+      },
+      {
+        questionType: "mcq",
+        question: "What is the circumference of a circle with radius 5 units?",
+        options: ["10π", "15π", "20π", "25π"],
+        answer: "10π",
+        check: false,
+      },
+    ],
+  },
+];
+const headlogo=document.getElementById("head");
+const questioncon = document.getElementById("questions-container");
+const questionss = document.getElementById("questions");
+const questionselement = document.getElementById("questionelement");
+const answers = document.getElementById("answer");
+const nexted = document.getElementById("next");
+const ans = document.getElementById("rightorwrong");
+const optionButtons = document.getElementsByClassName("option");
+let selectedOptionElement = document.getElementById("selectedOption");
+let grouping = document.getElementsByClassName("card");
+let maine = document.getElementById("mainee");
+let gkques = document.getElementById("gk");
+let aiques = document.getElementById("ai");
+let scques = document.getElementById("sc");
+let maques = document.getElementById("ma");
+let selectedOption;
+let sectionIndex;
+let currentIndex = 0;
+let score = 0;
+gkques.addEventListener("click", function () {
+  sectionIndex = 0;
+  questioncon.style.display = "";
+  headlogo.style.display="none";
+  for (let x = 0; x < grouping.length; x++) {
+    grouping[x].style.display = "none";
+  }
+  showQuestion();
+});
+aiques.addEventListener("click", function () {
+  sectionIndex = 1;
+  questioncon.style.display = "";
+  headlogo.style.display="none";
+  for (let x = 0; x < grouping.length; x++) {
+    grouping[x].style.display = "none";
+  }
+  showQuestion();
+});
+scques.addEventListener("click", function () {
+  sectionIndex = 2;
+  questioncon.style.display = "";
+  headlogo.style.display="none";
+  for (let x = 0; x < grouping.length; x++) {
+    grouping[x].style.display = "none";
+  }
+  showQuestion();
+});
+maques.addEventListener("click", function () {
+  sectionIndex = 3;
+  questioncon.style.display = "";
+  headlogo.style.display="none";
+  for (let x = 0; x < grouping.length; x++) {
+    grouping[x].style.display = "none";
+  }
+  showQuestion();
+});
+nexted.addEventListener("click", function () {
+  currentIndex++;
+  if (currentIndex === quizData[sectionIndex].questions.length) {
+    let scoring = document.createElement("p");
+    let complete = document.createElement("p");
+    let homebtn = document.createElement("button");
+    complete.innerHTML = "Quiz Completed";
+    scoring.innerHTML = "Your Score Is: " + score;
+    homebtn.innerHTML = "Go To Home";
+    homebtn.style.width = "300px";
+    homebtn.style.height = "100px";
+    homebtn.style.backgroundColor = "green";
+    questionselement.appendChild(complete);
+    questionselement.appendChild(scoring);
+    questionselement.appendChild(homebtn);
+    selectedOptionElement.style.display = "none";
+    nexted.style.display = "none";
+    questionss.style.display = "none";
+    for (let j = 0; j < optionButtons.length; j++) {
+      optionButtons[j].style.display = "none";
+    }
+    homebtn.addEventListener("click", function () {
+      location.reload();
+    });
+  } else {
+    showQuestion();
+    selectedOptionElement.innerHTML = "";
+  }
+});
+function showQuestion() {
+  let questionDisplay = quizData[sectionIndex].questions[currentIndex];
+  let questionNumber = currentIndex + 1;
+  questionss.innerHTML = questionNumber + ". " + questionDisplay.question;
+  for (let i = 0; i < optionButtons.length; i++) {
+    if (questionDisplay.options[i]) {
+      optionButtons[i].innerHTML = questionDisplay.options[i];
+      optionButtons[i].style.display = "";
+    } else {
+      optionButtons[i].style.display = "none";
+    }
+  }
+  for (let i = 0; i < optionButtons.length; i++) {
+    optionButtons[i].style.backgroundColor = "";
+    for (let j = 0; j < optionButtons.length; j++) {
+      optionButtons[j].disabled = false;
+    }
+    optionButtons[i].addEventListener("click", function () {
+      for (let i = 0; i < optionButtons.length; i++) {  
+        optionButtons[i].disabled = true;
+      }
+      if (optionButtons[i].innerHTML === questionDisplay.answer) {
+        selectedOptionElement.innerHTML =
+          "Correct answer! " + optionButtons[i].innerHTML;
+        selectedOptionElement.style.color = "greenyellow";
+        selectedOptionElement.style.fontSize = "40px";
+        optionButtons[i].style.backgroundColor = "green";
+        score++;
+      } else {
+        selectedOptionElement.innerHTML =
+          "Wrong answer! The Correct answer is: " + questionDisplay.answer;
+        selectedOptionElement.style.color = "red";
+        selectedOptionElement.style.fontSize = "40px";
+        optionButtons[i].style.backgroundColor = "red";
+        for(let i=0;i<optionButtons.length;i++){
+        if(optionButtons[i].innerHTML === questionDisplay.answer)
+          optionButtons[i].style.backgroundColor = "green";
+          optionButtons[i].style.display="";
+        }
+      }
+    });
+  }
+}
