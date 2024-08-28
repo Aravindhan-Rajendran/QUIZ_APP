2e82ea5bd3c81cb153391c83c07f922fe148f3a8
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-28 10:34:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -359,9 +359,10 @@ let sectionIndex;
 let currentIndex = 0;
 let score = 0;
 let btns=document.getElementById("btns");
+let main=document.getElementById("mainee");
 btns.addEventListener("click",function(){
-  
-})
+  main.style.backgroundColor="black";
+});
 gkques.addEventListener("click", function () {
   sectionIndex = 0;
   questioncon.style.display = "";
